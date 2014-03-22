96565f9796e41b0c8174280ab9bcf56177307e5d
Anderson Mesquita <andersonvom@gmail.com>
2014-03-22 10:53:23 -0400

    Add Runner basic attributes

--- js/runner.js
+++ js/runner.js
@@ -1,2 +1,7 @@
 function Runner(num_matches, strategy, game_manager) {
+  this.num_matches = num_matches;
+  this.strategy = strategy;
+  this.game_manager = game_manager;
+  this.play_delay = 100;
+  this.new_game_delay = 500;
 }
