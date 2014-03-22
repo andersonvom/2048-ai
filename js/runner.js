503d266b2805a1bdaf932f53cb9d74c0ffc9a485
Anderson Mesquita <andersonvom@gmail.com>
2014-03-22 11:03:08 -0400

    Add Runner.start()

--- js/runner.js
+++ js/runner.js
@@ -5,3 +5,16 @@ function Runner(num_matches, strategy, game_manager) {
   this.play_delay = 100;
   this.new_game_delay = 500;
 }
+
+Runner.prototype.start = function() {
+  if (this.num_matches > 0) {
+    console.log("Starting Match #" + this.num_matches);
+    this.game_manager.restart();
+    this.play();
+  } else {
+    console.log("No more matches! The end.");
+  }
+}
+
+Runner.prototype.play = function() {
+}
